fc420760ddcfef0a33e3b1074ef7246324e1266a
israelurcola <urcolaisrael@gmail.com>
2023-07-14 10:46:34 -0300

    una vez mas

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const app = express();
+
+const path = require('path'); 
+app.use(express.static('public'));
+
+
+app.get('/',function(req,res) {
+    res.send("betaHome");});
+
+app.get('/betaHome', (req,res) => {
+    res.sendFile (path.join(__dirname, '/views/betaHome.html'))});
+
+
+
+app.listen(3040 ,() => console.log('sevidor corriendo beta'));
